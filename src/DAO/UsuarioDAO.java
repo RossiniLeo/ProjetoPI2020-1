7234b91a8c9c9e20336145ef2effe85a36975714
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2020-04-14 17:30:13 -0300

    Initial Commit

---
+++ src/DAO/UsuarioDAO.java
@@ -0,0 +1,60 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import DAO.ConnectionFactory;
+import Model.Usuario;
+
+public class UsuarioDAO {
+	
+	public int criar(Usuario Usuario) {
+		String sqlInsert = "INSERT INTO usuario VALUES(null, ?, ?, ?)";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+			stm.setString(1, Usuario.getNome());
+			stm.setString(2, Usuario.getEmail());
+			stm.setString(3, Usuario.getSenha());
+			stm.execute();
+			String sqlQuery  = "SELECT LAST_INSERT_ID()";
+			try(PreparedStatement stm2 = conn.prepareStatement(sqlQuery);
+				ResultSet rs = stm2.executeQuery();) {
+				if(rs.next()){
+					Usuario.setUserID(rs.getInt(1));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return Usuario.getUserID();
+	}
+	
+	
+	public Usuario carregar(int id) {
+		Usuario Usuario = null;
+		String sqlSelect = "SELECT * FROM Usuario WHERE Usuario.id = ?";
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, id);
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					Usuario = new Usuario(id, rs.getString("nome"), rs.getString("email"), rs.getString("senha"));
+				} else {
+					Usuario = new Usuario(-1,null,null,null);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		
+		return Usuario;
+	}
+	
+}
---
+++ src/Model/Usuario.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class Usuario {
+	
+	private int userID;
+	private String nome;
+	private String email;
+	private String senha;
+	
+	public Usuario(int userID, String nome, String email, String senha) {
+		setUserID(userID);
+		setNome(nome);
+		setEmail(email);
+		setSenha(senha);
+	}
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getUserID() {
+		return userID;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+}
