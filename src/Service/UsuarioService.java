73502002900b617e0fccdbee5d97dd2df7c98247
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2020-04-14 17:34:44 -0300

    Service criado

---
+++ src/Service/UsuarioService.java
@@ -0,0 +1,16 @@
+package Service;
+
+import DAO.UsuarioDAO;
+import Model.Usuario;
+
+public class UsuarioService {
+	private UsuarioDAO usuarioDao = new UsuarioDAO();
+	
+	public void criar(Usuario usuario) {
+		usuarioDao.criar(usuario);
+	}
+	
+	public Usuario carregar(int id) {
+		return usuarioDao.carregar(id);
+	}
+}
